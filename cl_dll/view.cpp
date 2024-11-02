0ac7a3e55ad91af8e19972599b0f5ddf98fd2f34
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2024-11-02 23:28:16 +0000

    Remove weapon shifting effect

--- cl_dll/view.cpp
+++ cl_dll/view.cpp
@@ -836,11 +836,6 @@ void V_CalcNormalRefdef ( struct ref_params_s *pparams )
 		VectorCopy(view->angles, view->curstate.angles);
 	}
 
-	// pushing the view origin down off of the same X/Z plane as the ent's origin will give the
-	// gun a very nice 'shifting' effect when the player looks up/down. If there is a problem
-	// with view model distortion, this may be a cause. (SJB).
-	view->origin[2] -= 1;
-
 	// fudge position around to keep amount of weapon visible
 	// roughly equal with different FOV
 	if (pparams->viewsize == 110)
